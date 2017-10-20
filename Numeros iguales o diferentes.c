cf3f9db76d45bb01ca5a533e95ae6a5546e640d5
biaanilo <31136905+biaanilo@users.noreply.github.com>
2017-10-19 21:56:58 -0500

    codigo

---
+++ Numeros iguales o diferentes.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+/*sergio*/
+int main() {
+	unsigned int a,b;
+	a=88;
+	b=88;
+	if (a==0){
+	if (b==0){
+		printf("a es igual a b");
+	}
+    }
+		if (a==0){
+		if (b!=0){
+		printf("b es mayor que a");
+	}
+    }
+           if (b==0){
+           if(a!=0){
+           	printf("a es mayor que b");
+           }
+		   }
+		 while (a!=0 && b!=0) {
+		 
+		 	a=a-1;
+		 	b=b-1;
+		 	 if (a==0){
+		 	 if (b==0){
+		 	 	printf("a es igual que b");
+		 	  }
+		 	  }
+		         if(a==0){
+		         if(b!=0){
+		         	printf("b es mayor que a");
+				 }
+				 }
+				  if (a!=0){
+				  if (b==0){
+				    printf("a es mayor que b");
+				}
+			    }
+				
+				    if(a!=0){
+				    if(b!=0){
+				}else{
+				}
+				}
+				     continue;
+				 }
+	           	}
+			
+			
+		
+	
+
